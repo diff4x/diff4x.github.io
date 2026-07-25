36ad9366e4d031913353ddcdd9aa3b87d057016d
diff4x <diff4x@gmail.com>
2026-07-25 23:28:35 +0800

    20260725232833

--- src/js/raw/sw.js
+++ src/js/raw/sw.js
@@ -1,6 +1,6 @@
 // 触发 SW 更新检查
-self.SW_VERSION = '1784993023233';
-importScripts('/src/js/core-list.js?v=1784993023233');
+self.SW_VERSION = '1784993284208';
+importScripts('/src/js/core-list.js?v=1784993284208');
 
 // 缓存池隔离命名
 const CACHE_NAME_CORE = 'core-cache-' + BUILD_VERSION;
