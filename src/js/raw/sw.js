732fab8ac1b5c04150076a602f30c571c66d8814
diff4x <diff4x@gmail.com>
2026-08-25 22:39:11 +0800

    20260825223910

--- src/js/raw/sw.js
+++ src/js/raw/sw.js
@@ -1,6 +1,6 @@
-importScripts('/src/js/core-list.js?v=1787575578336');
+importScripts('/src/js/core-list.js?v=1787668748537');
 
-self.SW_VERSION = '1787575578336';
+self.SW_VERSION = '1787668748537';
 self.EMERGENCY = 'repair_command_id=2';
 
 const CACHE_NAME_CORE = 'core-cache-' + BUILD_VERSION;
